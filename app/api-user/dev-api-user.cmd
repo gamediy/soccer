4b023c7f821af4af34c6897ec0a4577cab6b5864
abri <abri@gmail.com>
2023-08-20 16:00:13 +0800

    api-user build

---
+++ app/api-user/dev-api-user.cmd
@@ -0,0 +1,6 @@
+ssh root@8.219.177.71 "bash -c 'cd /root/soccer/app/api-user && echo -e "1"|sh publish_dev.sh'"
+
+
+
+
+
--- app/api-user/publish_dev.sh
+++ app/api-user/publish_dev.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 read -p "请输入版本号: " ver
-name="api-soccer-user"
+name="api-user"
 image_name="$name:$ver"
 config_file="config.yaml"
 # 强制拉群 最近 GIT 代码
 git fetch --all
-git reset --hard origin/main
+git reset --hard origin/master
 
 # 编译 Go 代码
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o admin main.go
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o api-user main.go
 # 构建 Docker 镜像
 docker build -t "$image_name" .
 
 # 删除旧的 Docker 容器
 docker rm -f "$name" >/dev/null 2>&1
 # 运行 Docker 容器
-docker run -d --name "$name" -p 9000:9000 "$image_name" --gf.gcfg.file="config-dev.yaml"
+docker run -d --name "$name" -p 4101:4101 "$image_name" --gf.gcfg.file="config-dev.yaml"
