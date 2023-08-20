289373e5176a70421469fdeeb50ec13bbf95e477
abri <abri@gmail.com>
2023-08-20 14:37:48 +0800

    builder

--- app/admin/dev.cmd
+++ app/admin/dev.cmd
@@ -1,4 +1,4 @@
-ssh root@104.233.171.253 "bash -c 'cd /root/star_net/backend/app/admin && echo -e "1"|sh publish_dev.sh'"
+ssh root@http://8.219.177.71/ "bash -c 'cd /root/soccer/app/admin && echo -e "1"|sh publish_dev.sh'"
 
 
 
