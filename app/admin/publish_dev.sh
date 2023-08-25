578851892943c3ad6a90a65f9032d7964d68c425
whisper <lastwhisper2023@gmail.com>
2023-08-25 13:24:29 +0800

    update config

--- app/admin/publish_dev.sh
+++ app/admin/publish_dev.sh
@@ -2,6 +2,7 @@
 read -p "请输入版本号: " ver
 name="soccer-admin"
 image_name="$name:$ver"
+port=4100
 # 强制拉群 最近 GIT 代码
 git fetch --all
 git reset --hard origin/master
@@ -14,4 +15,4 @@ docker build -t "$image_name" .
 # 删除旧的 Docker 容器
 docker rm -f "$name" >/dev/null 2>&1
 # 运行 Docker 容器
-docker run -d --name "$name" -p 9000:9000 "$image_name" --gf.gcfg.file="config-dev.yaml"
+docker run -d --name "$name" -p "$port":"$port" "$image_name" --gf.gcfg.file="config-dev.yaml"
