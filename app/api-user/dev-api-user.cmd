639d1eb0dfd133d6d1f209916c59b7b53f9fd1e2
abri <abri@gmail.com>
2023-08-25 19:00:32 +0800

    play

--- app/api-user/dev-api-user.cmd
+++ app/api-user/dev-api-user.cmd
@@ -1,4 +1,4 @@
-ssh root@8.219.177.71 "bash -c 'cd /root/soccer/app/api-user && echo -e "1"|sh publish_dev.sh'"
+ssh root@8.219.177.71 "bash -c 'cd /root/soccer/backend/app/api-user && echo -e "1"|sh publish_dev.sh'"
 
 
 
