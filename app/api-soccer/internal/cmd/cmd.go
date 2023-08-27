d3bba63e7f355469978b52ce635f366c09e33b11
abri <abri@gmail.com>
2023-08-27 20:33:31 +0800

    api doc

--- app/api-soccer/internal/cmd/cmd.go
+++ app/api-soccer/internal/cmd/cmd.go
@@ -45,26 +45,23 @@ const (
 <head>
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <meta name="description" content="SwaggerUI"/>
+  <meta
+    name="description"
+    content="SwaggerUI"
+  />
   <title>SwaggerUI</title>
-  <link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist@latest/swagger-ui.css" />
+  <link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist@4.5.0/swagger-ui.css" />
 </head>
 <body>
 <div id="swagger-ui"></div>
-<script src="https://unpkg.com/swagger-ui-dist@latest/swagger-ui-bundle.js" crossorigin></script>
+<script src="https://unpkg.com/swagger-ui-dist@4.5.0/swagger-ui-bundle.js" crossorigin></script>
 <script>
-	window.onload = () => {
-		window.ui = SwaggerUIBundle({
-			url:    '/api/soccer/doc/api.json',
-			dom_id: '#swagger-ui',
-			   requestInterceptor: (req) => {
-                    req.headers['Authorization'] = 'Bearer your-auth-token';
-                    return req;
-                },
-		});
-
-
-	};
+  window.onload = () => {
+    window.ui = SwaggerUIBundle({
+      url: '/api/soccer/doc/api.json',
+      dom_id: '#swagger-ui',
+    });
+  };
 </script>
 </body>
 </html>
