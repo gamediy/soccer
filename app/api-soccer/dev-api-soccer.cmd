551b928f93ea90885e51e47904a8064e9385e173
abri <abri@gmail.com>
2023-09-07 18:35:47 +0800

    批量下注

--- app/api-soccer/dev-api-soccer.cmd
+++ app/api-soccer/dev-api-soccer.cmd
@@ -1,7 +1,7 @@
 ssh root@8.219.177.71 "bash -c 'cd /root/soccer/backend/app/api-soccer && echo -e "1"|sh publish_dev.sh'"
 
 
-ssh root@8.219.177.71 
-
+ssh root@8.219.177.71
+ssh root@8.219.177.71 "cd /root/soccer/backend/app/api-soccer"
 
 
