7c51fc11f03185dc1ce8caca970701b89b082cee
abri <abri@gmail.com>
2023-09-08 19:31:23 +0800

    批量下注

--- app/api-soccer/api/order/bet.go
+++ app/api-soccer/api/order/bet.go
@@ -6,14 +6,14 @@ import (
 )
 
 type OrderBetReq struct {
-	g.Meta `tags:"订单" method:"get" path:"/bet" dc:"投注"`
+	g.Meta `tags:"订单" method:"post" path:"/bet" dc:"投注"`
 	ordersvc.BetInput
 }
 type OrderBetRes struct {
 }
 
 type OrderBetBatchReq struct {
-	g.Meta `tags:"订单" method:"get" path:"/bet_batch" dc:"投注批量"`
+	g.Meta `tags:"订单" method:"post" path:"/bet_batch" dc:"投注批量"`
 	ordersvc.BetBatchInput
 }
 type OrderBetBatchRes struct {
