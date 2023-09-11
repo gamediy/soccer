e7b0c4c4c23ad6a94359cd896d2ff3d05b43a975
abri <abri@gmail.com>
2023-09-11 21:14:42 +0800

    提交充值

--- utility/utils/xip/ip.go
+++ utility/utils/xip/ip.go
@@ -1,10 +1,26 @@
 package xip
 
 import (
+	"context"
 	"fmt"
 	"github.com/gocolly/colly/v2"
+	"github.com/gogf/gf/v2/frame/g"
 	"github.com/gogf/gf/v2/text/gstr"
+	"github.com/gogf/gf/v2/util/gconv"
 )
+
+type Location struct {
+	IP       string `json:"ip"`
+	Hostname string `json:"hostname"`
+	Anycast  bool   `json:"anycast"`
+	City     string `json:"city"`
+	Region   string `json:"region"`
+	Country  string `json:"country"`
+	Loc      string `json:"loc"`
+	Org      string `json:"org"`
+	Postal   string `json:"postal"`
+	Timezone string `json:"timezone"`
+}
 
 func GetIpInfo(ip string) (string, error) {
 	c := colly.NewCollector()
@@ -18,4 +34,19 @@ func GetIpInfo(ip string) (string, error) {
 	}
 	c.Wait()
 	return gstr.Trim(location), nil
+}
+
+func GetIpInfoIo(ctx context.Context, ip string) Location {
+	loc := Location{}
+	get, err := g.Client().Get(ctx, fmt.Sprintf("https://ipinfo.io/%s?token=752e995a5aa063", ip))
+	if err != nil {
+		g.Log().Error(ctx, err)
+		return loc
+	}
+	allString := get.ReadAllString()
+	gconv.Struct(allString, &loc)
+
+	defer get.Close()
+
+	return loc
 }
--- utility/utils/xip/ip_test.go
+++ utility/utils/xip/ip_test.go
@@ -1,14 +1,13 @@
 package xip
 
 import (
+	"context"
 	"github.com/gogf/gf/v2/frame/g"
 	"testing"
 )
 
 func TestGetIpInfo(t *testing.T) {
-	location, err := GetIpInfo("102.215.252.23")
-	if err != nil {
-		t.Fatal(err)
-	}
+	location := GetIpInfoIo(context.TODO(), "102.215.252.23")
+
 	g.Dump(location)
 }
