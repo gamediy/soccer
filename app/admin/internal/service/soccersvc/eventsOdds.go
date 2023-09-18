a37cddea025d4fe1df2a133cfbb1cc5826b005f9
whisper <lastwhisper2023@gmail.com>
2023-09-18 14:40:11 +0800

    change add events odds

--- app/admin/internal/service/soccersvc/eventsOdds.go
+++ app/admin/internal/service/soccersvc/eventsOdds.go
@@ -12,13 +12,14 @@ type BatchInsert struct {
 
 func (s BatchInsert) Exec(ctx context.Context) error {
 	for _, i := range s.Data {
-		count, err := dao.EventsOdds.Ctx(ctx).Count("bout_status = ? and play_code = ? and play_type_code = ?", i.BoutStatus, i.PlayCode, i.PlayTypeCode)
+		count, err := dao.EventsOdds.Ctx(ctx).Count("events_id = ? and bout_status = ? and play_code = ? and play_type_code = ?", i.EventsId, i.BoutStatus, i.PlayCode, i.PlayTypeCode)
 		if err != nil {
 			return err
 		}
 		if count != 0 {
 			continue
 		}
+		i.Id = 0
 		if _, err = dao.EventsOdds.Ctx(ctx).Insert(i); err != nil {
 			return err
 		}
